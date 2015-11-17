11b806f24a363b5c979e5d6ebe2ff35d4f785029
fraserxu <xvfeng123@gmail.com>
2015-11-17 14:24:12 +0800

    prepare for heroku

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # slack-translate
 A simple node server use slash command to do translation
+
--- index.js
+++ index.js
@@ -47,9 +47,10 @@ app.get('/', function (req, res) {
   })
 });
 
-var server = app.listen(3000, function () {
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
   var host = server.address().address;
-  var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
