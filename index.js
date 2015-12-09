76bf889b27a200800d1db7eae2a374066179a7d0
fraserxu <xvfeng123@gmail.com>
2015-12-09 11:56:14 +1100

    add tests

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-require('newrelic');
+require('newrelic')
 
-var express = require('express');
-var request = require('request');
-var app = express();
+var express = require('express')
+var request = require('request')
+var app = express()
 
 var youdaoApi = 'http://fanyi.youdao.com/openapi.do?keyfrom=fanyi-node&key=593554388&type=data&doctype=json&version=1.1&q='
 
 app.get('/', function (req, res) {
-  var q = req.query.text;
+  var q = req.query.text
 
   if (!q || q === '') {
     res.send({
-      "response_type": "in_channel",
-      "text": "Please use '/translate [phrase]' ."
-    });
+      'response_type': 'in_channel',
+      'text': "Please use '/translate [phrase]' ."
+    })
     return
   }
 
-  var uri = youdaoApi + encodeURIComponent(q);
+  var uri = youdaoApi + encodeURIComponent(q)
 
   request.get(uri, function (err, response, body) {
-    var body = JSON.parse(body)
+    if (err) {
+      res.send({
+        'response_type': 'in_channel',
+        'text': 'Server error.'
+      })
+    }
+    body = JSON.parse(body)
 
     if (body.translation) {
-
       var data = {
-        "response_type": "in_channel",
-        "text": "The translation for " + q + " is " + body.translation + ' .'
+        'response_type': 'in_channel',
+        'text': 'The translation for ' + q + ' is ' + body.translation + ' .'
       }
 
       if (body.basic && body.basic.explains) {
         data.attachments = [
           {
-            "text": body.basic.explains.reduce(function(a, b) {
+            'text': body.basic.explains.reduce(function (a, b) {
               return a + '\n' + b
-            }, "")
+            }, '')
           }
         ]
       }
 
-      res.send(data);
+      res.send(data)
     } else {
       res.send({
-        "response_type": "in_channel",
-        "text": "Sorry, I can not find the translation for " + q + '.'
-      });
+        'response_type': 'in_channel',
+        'text': 'Sorry, I can not find the translation for ' + q + '.'
+      })
     }
   })
-});
+})
 
-
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
-  var host = server.address().address;
+  var host = server.address().address
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+  console.log('Example app listening at http://%s:%s', host, port)
+})
